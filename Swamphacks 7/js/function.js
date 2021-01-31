99fb36639699bc3939316f28f32d5402494d2bcf
Zachary <75510412+Zachary1575@users.noreply.github.com>
2021-01-31 04:39:53 -0500

    Delete Swamphacks 7 directory

--- Swamphacks 7/js/function.js
+++
@@ -1,6 +0,0 @@
-function Main() {
-    var x = document.getElementById("main");
-        x.style.display = "block";
-    var x = document.getElementById("index");
-    x.style.display = "none";
-  }
